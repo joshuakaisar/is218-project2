99620de206ecaa9eb1b6d51beff1fd2d59f1886c
joshuakaisar <kaisarjoshua@gmail.com>
2022-03-08 16:57:25 -0500

    updated both files for new navbar

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,9 +6,9 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
     assert b'<a class="nav-link" href="/page1">GIT</a>' in response.data
-    assert b'<a class="nav-link" href="/page2">Page 2</a>' in response.data
-    assert b'<a class="nav-link" href="/page3">Page 3</a>' in response.data
-    assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
+    assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
 
 def test_request_index(client):
     """This makes the index page"""
