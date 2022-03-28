d28de6dee274f7ec31f44dffaa77725ed143f10b
Joshua Kaisar <joshuakaisar@joshuas-mbp.resource.campus.njit.edu>
2022-03-28 12:40:52 -0400

    fixed potential issue

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -54,5 +54,5 @@ def test_request_page5(client):
 
 def test_request_page_not_found(client):
     """This makes the index page"""
-    response = client.get("/page5")
+    response = client.get("/page6")
     assert response.status_code == 404
